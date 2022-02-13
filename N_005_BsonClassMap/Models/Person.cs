c7e3676273b19d190112e1ad118d09aafa9e1fc6
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-13 22:06:41 +1000

    Добавил пример использования BsonClassMap

---
+++ N_005_BsonClassMap/Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace N_005_BsonClassMap.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ N_005_BsonClassMap/Program.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using N_005_BsonClassMap.Models;
+
+namespace N_005_BsonClassMap;
+
+class Program
+{
+    public static void Main()
+    {
+        BsonClassMap.RegisterClassMap<Person>(classMap =>
+        {
+            classMap.AutoMap();
+            classMap.MapMember(person => person.Name).SetElementName("firstName");
+        });
+
+        Person person = new Person
+        {
+            Name = "Bill",
+            Age = 48
+        };
+
+        BsonDocument doc = person.ToBsonDocument();
+
+        Console.WriteLine(doc);
+
+        Console.ReadKey();
+    }
+}
