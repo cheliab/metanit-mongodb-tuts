78f2b02fcec043ad4ad079d5e02b0e162fcaa43f
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-14 12:34:03 +1000

    Пример использования конвенции

---
+++ N_006_ConventionPack/Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace N_006_ConventionPack.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ N_006_ConventionPack/Program.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using N_006_ConventionPack.Models;
+
+namespace N_006_ConventionPack;
+
+class Program
+{
+    public static void Main()
+    {
+        ConventionPack conventionPack = new ConventionPack();
+        conventionPack.Add(new CamelCaseElementNameConvention());
+        ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+        Person person = new Person
+        {
+            Name = "Pavel",
+            Age = 31
+        };
+
+        BsonDocument doc = person.ToBsonDocument();
+
+        Console.WriteLine(doc);
+
+        Console.ReadKey();
+    }
+}
+
