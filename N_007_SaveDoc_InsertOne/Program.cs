acb3091eb100bc143a7b7d3643eeb410ea7b703d
Berezkin Pavel <cheliabkns@gmail.com>
2022-02-14 13:48:22 +1000

    Добавил пример сохранения документа в БД

---
+++ N_007_SaveDoc_InsertOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace N_007_SaveDoc_InsertOne;
+
+class Program
+{
+    public static async Task Main()
+    {
+        await SaveDoc_InsertOne();
+    }
+
+    private static async Task SaveDoc_InsertOne()
+    {
+        string connection = "mongodb://localhost";
+        var client = new MongoClient(connection);
+
+        var database = client.GetDatabase("test");
+        var collection = database.GetCollection<BsonDocument>("people");
+
+        BsonDocument personDoc = new BsonDocument
+        {
+            {"Name", "Pavel"},
+            {"Age", 32},
+            {"Languages", new BsonArray{"english", "german"}}
+        };
+
+        await collection.InsertOneAsync(personDoc);
+    }
+
+    private static async Task SaveDoc_InsertMany()
+    {
+        string connection = "mongodb://localhost";
+        var client = new MongoClient(connection);
+        
+        var database = client.GetDatabase("test");
+        var collection = database.GetCollection<BsonDocument>("people");
+
+        BsonDocument billDoc = new BsonDocument
+        {
+            {"Name", "Bill"},
+            {"Age", 32},
+            {"", new BsonArray{}}
+        };
+    }
+}
